e00acfa67bf62fb121b22c72bbf0e99b9c9777fa
javier123454321 <javier.e.glz@gmail.com>
2026-02-04 04:38:52 +0000

    prd

--- scripts/adusa/add_ticket_to_refinement.sh
+++
@@ -1,9 +0,0 @@
-if [[ $1 == https://* ]]; then
-  ticket=$(echo $1 | awk -F'/' '{print $NF}')
-  printf "$(date +%Y-%m-%d) // $ticket\n[ ] - $1\n$(cat $script_dir/refinement/tickets.txt)" > $script_dir/refinement/tickets.txt
-  echo "Ticket $ticket added to refinement"
-else
-  printf "$(date +%Y-%m-%d) // $1\n[ ] - https://jira-us-aholddelhaize.atlassian.net/browse/$1\n$(cat $script_dir/refinement/tickets.txt)" > $script_dir/refinement/tickets.txt
-  echo "Ticket $1 added to refinement"
-fi
-
--- scripts/adusa/run_dev.sh
+++
@@ -1,7 +0,0 @@
-#/usr/local/bin/bash
-if [[ -z $1 ]] ; then
-  yarn run dev
-else
-  echo "Running settings for $1"
-  yarn run dev --dotenv $HOME/.scratch/.env.$1
-fi
